ec0cc7121ff2b2c9caa6240565c27cb2a454075a
Gal Agai <gal@nbv.co.il>
2021-09-03 18:58:16 +0300

    First Step

---
+++ src/TodoForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodoForm = () => {
+  return (
+    <div className='info'>
+      <input type='text' autoFocus />
+    </div>
+  );
+};
+
+export default TodoForm;
---
+++ src/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+
+const TodoList = () => {
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <TodoItem />
+    </div>
+  );
+};
+
+export default TodoList;
